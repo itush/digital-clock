cf3489341440e21eb95310a579c846de2e151f02
itush <itusharbiswas@gmail.com>
2022-02-09 23:41:49 +0530

    dev-com-1

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,6 +6,20 @@ var m = date.getMinutes();
 var s = date.getSeconds();
 var session = "AM";
 
+
+if(h==0){
+    h=12;
+}
+
+if(h>12){
+    h = h-12;
+    session ="PM"
+}
+
+h = (h<10) ? "0" + h:h;
+m = (m<10) ? "0" + m:m;
+s = (s<10) ? "0" + s:s;
+
 document.getElementById("dclock").innerHTML= h+":"+m+":"+s+" "+session;
 
 setTimeout(currentTime, 1000)
